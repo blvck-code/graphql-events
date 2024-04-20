3290ea28920932c8b87c82bc2e41196cf5232d8c
Maurice Oluoch <53207394+blvck-code@users.noreply.github.com>
2024-04-20 09:54:40 +0300

    added types

--- app.js
+++ app.js
@@ -5,18 +5,37 @@ const { buildSchema } = require("graphql");
 
 const app = express();
 
+const events = [];
+
 app.use(bodyParser.json());
 
 app.use(
   "/graphql",
   graphqlHTTP({
     schema: buildSchema(`
+        input EventInput {
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
+        type Event {
+            _id: ID!
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
         type RootQuery {
-            events: [String!]!
+            events: [Event!]!
         }
+
         type RootMutation {
-            createEvent(name: String): String
+            createEvent(eventInput: EventInput): Event
         }
+
         schema {
             query: RootQuery
             mutation: RootMutation
@@ -24,11 +43,19 @@ app.use(
     `),
     rootValue: {
       events: () => {
-        return ["Romantic Cooking", "Sailing", "All-Night Coding"];
+        return events;
       },
       createEvent: (args) => {
-        const eventName = args.name;
-        return eventName;
+        const event = {
+          _id: Math.random().toString(),
+          title: args.eventInput.title,
+          description: args.eventInput.description,
+          price: +args.eventInput.price,
+          date: new Date().toISOString(),
+        };
+        console.log(args);
+        events.push(event);
+        return event;
       },
     },
     graphiql: true,
